c0da5aa6c8688f443ef5b81d49b899ab0e8f585f
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-01 14:09:14 -0700

    initial set up and creation of wrapper

--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,5 +1,15 @@
 require 'httparty'
 
 class SlackApiWrapper
-  # Your code here!
+	URL = "https://slack.com/api/"
+	TOKEN = ENV["SLACK_TOKEN"]
+
+	def self.list_channels
+		response = HTTParty.get("#{URL}channels.list?token=#{TOKEN}")
+		if response["channels"] # if statement just in case something went wrong.
+			return response["channels"] # response.parsed_response["channels"]
+		else
+			return []
+		end
+	end
 end
