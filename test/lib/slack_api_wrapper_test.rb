fbc7e468dd26e1a204a73f9d82fe4c1ed2ff375b
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-02 10:26:47 -0700

    made initial slack api wrapper cassette test

--- test/lib/slack_api_wrapper_test.rb
+++ test/lib/slack_api_wrapper_test.rb
@@ -2,4 +2,13 @@ require 'test_helper'
 
 describe SlackApiWrapper do
 
+	it "Can send valid message to real channel" do
+		message = "test message"
+	  VCR.use_cassette("channels") do
+	    response = SlackApiWrapper.send_message("CAGCPB64A", message)
+	    # response["ok"].must_equal true
+	    # response["message"]["text"].must_equal message
+	  end
+	end
+
 end
