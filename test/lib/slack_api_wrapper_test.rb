72161aeace4caf8a3c214580bd6f20fbbc1c224d
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-02 10:51:10 -0700

    added second test (failure for invalid slack channel) for slack api wrapper

--- test/lib/slack_api_wrapper_test.rb
+++ test/lib/slack_api_wrapper_test.rb
@@ -11,8 +11,15 @@ describe SlackApiWrapper do
 			# response.must_equal true
 			response["ok"].must_equal true
 	    response["message"]["text"].must_equal message
-
 	  end
 	end
 
+	it "Can't send message to fake channel" do
+	  VCR.use_cassette("channels") do
+	    response = SlackApiWrapper.send_message("this-channel-does-not-exist",
+				"test message")
+	    response["ok"].must_equal false
+	    response["error"].wont_be_nil
+	  end
+	end
 end
